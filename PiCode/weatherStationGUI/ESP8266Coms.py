96f721cf0874013c0077ea9c0e930798790ba5f9
hamjared <38144214+hamjared@users.noreply.github.com>
2019-04-27 14:12:55 -0600

    Update ESP8266Coms.py

--- PiCode/weatherStationGUI/ESP8266Coms.py
+++ PiCode/weatherStationGUI/ESP8266Coms.py
@@ -1,7 +1,21 @@
 import socket
 import json
 
-WeatherStationIP = "192.168.1.149"
+def findStationIP():
+    pid = Popen(["arp", "-n"], stdout=PIPE)
+    s = pid.communicate()[0]
+    t = s.decode("utf-8")
+    u = t.split('\n')
+    
+    for i in range(1, len(u)-1):
+        try:
+            mac = re.search(r"(([a-f\d]{1,2}\:){5}[a-f\d]{1,2})", u[i]).groups()[0]
+        except AttributeError as AE:
+            continue
+        if mac == 'bc:dd:c2:25:1b:24':
+            return(u[i].split(' ')[0])
+
+WeatherStationIP = findStationIP()
 WeatherStationPort = 4210
 
 def sendData(message):
@@ -38,9 +52,11 @@ def getPressure():
 def main():
     sendData("Hello from Pi")
     data, addr = receiveData()
-    print(data)
-    jsonData = json.loads(data)
-    print(jsonData["temperature"])
+    #print(data)
+    jsonData = json.loads(data.decode("utf-8"))
+    print("Temperature: {}".format(jsonData["temperature"]))
+    print("Humidity: {}".format(jsonData["humidity"]))
+    print("Pressure: {}".format(jsonData["pressure"]))
 
 
 if __name__ == "__main__":
