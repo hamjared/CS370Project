c8b51123a49db77af2bca92245110b9d776d6ebc
Stephen Sutherland <haka24@charleston.cs.colostate.edu>
2019-05-01 13:24:05 -0600

    figures out city name by itself

--- PiCode/weatherStationGUI/weather/views.py
+++ PiCode/weatherStationGUI/weather/views.py
@@ -1,15 +1,34 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 import ESP8266Coms
+import location
 
 
 def index(request):
+    coords = location.getCoords()
+    formattedCoords = "({:.4f}, {:.4f})".format(coords['location']['lat'],coords['location']['lng'])
+    city_state = location.getCity(coords)
+    temp = ESP8266Coms.getTemp()
+    humidity = ESP8266Coms.getHumidity()
+    pressure = ESP8266Coms.getPressure()
+
+    if temp < 40:
+        description = "Cold"
+    elif temp < 60:
+        description = "Cool"
+    elif temp < 70:
+        description = "Warm"
+    else:
+        description = "Hot"
+
+
     weather = {
-        'city': "Ft Collins",
-        'temperature': ESP8266Coms.getTemp(),
-        'humidity': ESP8266Coms.getHumidity(),
-        'pressure': ESP8266Coms.getPressure(),
-        'description': "Sunny"
+        'city': city_state,
+        'temperature': temp,
+        'humidity': humidity,
+        'pressure': pressure,
+        'description': description,
+        'coords': formattedCoords
     }
     context = {'weather': weather}
     return render(request, 'weather/index.html', context) #returns the index.html template
